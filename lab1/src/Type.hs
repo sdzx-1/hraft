968f65d7c65d7850c505146478156d605ba604c0
EMQ-YangM <yangm@emqx.io>
2022-02-23 19:19:44 +0800

    init Type

--- lab1/src/Type.hs
+++ lab1/src/Type.hs
@@ -1,17 +1,26 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LinearTypes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+
 module Type where
 
-import           Data.Text
-import           Data.Text.IO
+import Process.HasServer
+import Process.Metric
+import Process.TH
+import Process.Type
+import Process.Util
 
+--- coordinator
+--- worker -- mapWork  reduceWork
 
+data AskWork where
+  MapWork :: ((FilePath, String) -> IO ()) -> RespVal () %1 -> AskWork
+  ReduceWork :: ((String, [String]) -> IO ()) -> RespVal () %1 -> AskWork
 
-
-
-
-
-
-
-
-
-
-
+mkSigAndClass
+  "SigCoord"
+  [ ''AskWork
+  ]
