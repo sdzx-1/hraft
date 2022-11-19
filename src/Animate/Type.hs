7112c17a645491745bfb21f4cf32a5a75c210a45
EMQ-YangM <yangm@emqx.io>
2022-11-19 13:47:27 +0800

    render recvMsg

--- src/Animate/Type.hs
+++ src/Animate/Type.hs
@@ -38,7 +38,7 @@ main = do
   alive_node_set_ref <- newIORef (Set.fromList $ Map.keys nodes)
   faults_info_ref <- newIORef ""
   animateIO
-    (InWindow "Nice Window" (1000, 1200) (10, 10))
+    (InWindow "Nice Window" (1200, 1400) (10, 10))
     white
     ( prodNextFrame
         nodes
@@ -88,21 +88,35 @@ prodNextFrame
       _ -> do
         let (ls', gs) = splitLessThanActions t [] actions
             ls = reverse ls'
+
+        let updateNodeState_map nid' uf = do
+              nodeState_map <- readIORef nodeState_map_ref
+              let nodestate = fromJust $ Map.lookup nid' nodeState_map
+              writeIORef nodeState_map_ref (Map.insert nid' (uf nodestate) nodeState_map)
+
         forM_ ls $ \action -> do
           case action of
             Action _ (nid, h) -> do
-              let updateNodeState_map nid' uf = do
-                    nodeState_map <- readIORef nodeState_map_ref
-                    let nodestate = fromJust $ Map.lookup nid' nodeState_map
-                    writeIORef nodeState_map_ref (Map.insert nid' (uf nodestate) nodeState_map)
               case h of
                 FollowerTimeoutToCandidate -> updateNodeState_map nid (\ns -> ns{role = Candidate})
-                FollowerRecvMsg{} -> updateNodeState_map nid (\ns -> ns{role = Follower})
                 CandidateElectionSuccess{} -> updateNodeState_map nid (\ns -> ns{role = Leader})
                 LeaderToFollowerAtAP{} -> updateNodeState_map nid (\ns -> ns{role = Follower})
                 LeaderToFollowerAtRV{} -> updateNodeState_map nid (\ns -> ns{role = Follower})
                 UpdateTermAndVotedFor term vf ->
                   updateNodeState_map nid (\ns -> ns{term = term, votedFor = vf})
+                FollowerRecvMsg (PeerNodeId pid, msg) ->
+                  updateNodeState_map
+                    nid
+                    ( \ns ->
+                        ns
+                          { role = Follower
+                          , recvMsg = show pid ++ ": " ++ showMsg msg
+                          }
+                    )
+                CandidateRecvMsg (PeerNodeId pid, msg) ->
+                  updateNodeState_map nid (\ns -> ns{recvMsg = show pid ++ ": " ++ showMsg msg})
+                LeaderRecvMsg (PeerNodeId pid, msg) ->
+                  updateNodeState_map nid (\ns -> ns{recvMsg = show pid ++ ": " ++ showMsg msg})
                 _ -> pure ()
             NetworkChangeAction _ k@(NetworkChange ps@(pl, _) _ _) -> do
               print k
@@ -118,6 +132,7 @@ prodNextFrame
               writeIORef alive_node_set_ref (Set.fromList $ Map.keys nodes)
               forM_ nids $ \(NodeId nid) -> do
                 modifyIORef' alive_node_set_ref (Set.delete nid)
+                updateNodeState_map nid (\ns -> ns{role = Follower, recvMsg = ""})
                 forM_ (Map.keys nodes) $ \p -> modifyIORef' nodeConnectSet_ref (Set.delete (nid, p) . Set.delete (p, nid))
 
         writeIORef actions_ref gs
@@ -129,10 +144,16 @@ prodNextFrame
           ( pictures
               [ renderNodeCommectSet nodeState_map nodeConnectSet'
               , rr alive_node_set nodeState_map
-              , translate 0 350 $ scale 0.3 0.3 $ text $ show t
-              , translate (-250) 550 $ scale 0.3 0.3 $ text $ "faults: " ++ faults_info
+              , translate 0 550 $ scale 0.3 0.3 $ text $ "elapsed time: " ++ show t
+              , translate (-250) 350 $ scale 0.4 0.4 $ color red $ text $ "faults: " ++ faults_info
               ]
           )
+
+showMsg :: Show s => Msg s -> String
+showMsg (MsgAppendEntries _ (AppendEntries{term, entries})) = "AppEnd: " ++ show term ++ ", " ++ show entries
+showMsg (MsgAppendEntriesResult _ AppendEntriesResult{success}) = "AppEndResult: " ++ show success
+showMsg (MsgRequestVote RequestVote{term, candidateId}) = "VoteReq: " ++ show term ++ ", " ++ show candidateId
+showMsg (MsgRequestVoteResult RequestVoteResult{voteGranted}) = "VoteReqResult: " ++ show voteGranted
 
 -- >>> nodeConnectMap
 
@@ -179,6 +200,7 @@ data NodeState = NodeState
   , position :: Point
   , term :: Term
   , votedFor :: Maybe Id
+  , recvMsg :: String
   }
 
 mkNodes :: Int -> Map Id NodeState
@@ -195,6 +217,7 @@ mkNodes len =
               )
               0
               Nothing
+              ""
           )
         | i <- [0 .. len - 1]
         ]
@@ -215,13 +238,23 @@ rr alive_node_set m =
           ls
 
 renderNodeState :: Set Id -> NodeState -> Picture
-renderNodeState alive_node_set NodeState{nodeId, role, position = (x, y), term, votedFor} =
-  translate x y $
-    pictures
-      [ if Set.member nodeId alive_node_set
-          then color (roleToColor role) $ circleSolid 10
-          else blank
-      , translate 10 0 $ scale 0.2 0.2 $ text $ show nodeId
-      , translate 10 (-20) $ scale 0.2 0.2 $ text $ "term: " ++ show term
-      , translate 10 (-40) $ scale 0.2 0.2 $ text $ "vote: " ++ show votedFor
-      ]
+renderNodeState
+  alive_node_set
+  NodeState
+    { nodeId
+    , role
+    , position = (x, y)
+    , term
+    , votedFor
+    , recvMsg
+    } =
+    translate x y $
+      pictures
+        [ translate 10 (-60) $ scale 0.15 0.15 $ rotate 45 $ text $ "recvMsg: " ++ recvMsg
+        , if Set.member nodeId alive_node_set
+            then color (roleToColor role) $ circleSolid 10
+            else blank
+        , translate 10 0 $ scale 0.2 0.2 $ text $ show nodeId
+        , translate 10 (-20) $ scale 0.2 0.2 $ text $ "term: " ++ show term
+        , translate 10 (-40) $ scale 0.2 0.2 $ text $ "vote: " ++ show votedFor
+        ]
